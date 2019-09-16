bd720345c6008aebedd7770ce6c6f6b7981b9576
Eric Schrock <Eric.Schrock@delphix.com>
2019-09-16 14:19:41 -0400

    first plan

---
+++ community/state.tf
@@ -0,0 +1,31 @@
+resource "aws_dynamodb_table" "state" {
+  name           = "${var.project}-state"
+  read_capacity  = "20"
+  write_capacity = "20"
+  hash_key       = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
+
+resource "aws_s3_bucket" "state" {
+  bucket = "${var.project}-state"
+
+  versioning {
+    enabled = true
+  }
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+
+  tags = {
+    Name = "${var.project}-state"
+  }
+}
