b60a7a4d806f96ba7cac97fe422cf376c3af4a32
Eric Schrock <Eric.Schrock@delphix.com>
2019-09-17 21:43:57 -0400

    switch to website endpoint

--- community/download.tf
+++ community/download.tf
@@ -23,16 +23,38 @@ resource "aws_s3_bucket" "download" {
   }
 }
 
+# Public access policy for all objects
+resource "aws_s3_bucket_policy" "download" {
+  bucket = "${aws_s3_bucket.download.id}"
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Sid": "AddPerm",
+      "Effect": "Allow",
+      "Principal": "*",
+      "Action": "s3:GetObject",
+
+      "Resource": "${aws_s3_bucket.download.arn}/*"
+    }
+  ]
+}
+EOF
+}
+
 # CloudFront
-resource "aws_cloudfront_distribution" "download-site" {
+resource "aws_cloudfront_distribution" "download" {
   provider              = "aws.us-east-1"
   origin {
-    domain_name         = "${aws_s3_bucket.download.bucket_domain_name}"
+    domain_name         = "${aws_s3_bucket.download.website_endpoint}"
     origin_id           = "${aws_s3_bucket.download.id}-origin"
   }
 
   enabled               = true
   aliases               = [ "download.titan-data.io" ]
+  default_root_object   = "index.html"
 
   default_cache_behavior {
     allowed_methods     = [ "DELETE", "GET", "HEAD", "OPTIONS", "PATCH",
@@ -64,5 +86,18 @@ resource "aws_cloudfront_distribution" "download-site" {
     acm_certificate_arn         = "${aws_acm_certificate_validation.main.certificate_arn}"
     ssl_support_method          = "sni-only"
     minimum_protocol_version    = "TLSv1.1_2016"
+  }
+}
+
+# DNS
+resource "aws_route53_record" "download" {
+  zone_id = "${aws_route53_zone.main.zone_id}"
+  name    = "download"
+  type    = "A"
+
+  alias {
+    name                   = "${aws_cloudfront_distribution.download.domain_name}"
+    zone_id                = "${aws_cloudfront_distribution.download.hosted_zone_id}"
+    evaluate_target_health = false
   }
 }
