cc3f3b5f32683342fb4c769955c83af53c12e802
Eric Schrock <Eric.Schrock@delphix.com>
2019-10-02 13:59:10 -0400

    add bucket for test data

--- community/titan-server.tf
+++ community/titan-server.tf
@@ -1,6 +1,6 @@
 #
-# Infrastructure required for titan-server automation. This is just a user
-# with permission to write objects to the maven bucket.
+# Infrastructure required for titan-server automation. This includes a user
+# with permission to write objects to the maven and test buckets.
 #
 
 resource "aws_iam_user" "titan-server" {
@@ -21,6 +21,25 @@ resource "aws_iam_user_policy" "titan-server-maven" {
       "Action": "s3:PutObject",
 
       "Resource": "${aws_s3_bucket.maven.arn}/*"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_user_policy" "titan-server-test" {
+  name = "titan-server-test"
+  user = "${aws_iam_user.titan-server.name}"
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Action": "s3:PutObject",
+
+      "Resource": "${aws_s3_bucket.test.arn}/*"
     }
   ]
 }
